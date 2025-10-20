07de501d7ea50f57a380b7649b86de1b6a0890cb
maharjanhudeo <maharjanhudeo@gmail.com>
2025-10-20 14:17:30 +0545

    Update internal page links

---
+++ assets/js/main.js
@@ -0,0 +1,204 @@
+jQuery(document).ready(function ($) {
+
+    var nav_offset_top = $("header").height() + 120;
+
+    function navbarFixed() {
+        if ($(".mt-header").length) {
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop(),
+                    slowscroll = scroll / 2;
+                if (scroll >= nav_offset_top) {
+                    $(".mt-header").addClass("header_fixed");
+
+                } else {
+                    $(".mt-header").removeClass("header_fixed");
+                    //   $('.billboard').css({
+                    //  transform: "translateY(0)"
+                    //  });
+                }
+            });
+        }
+    }
+
+    navbarFixed();
+    $(document).on('click', '.ham-menu', function () {
+        $('.mt-header__wrapper').toggleClass('active');
+        $('.ham-menu').toggleClass('active');
+        $(' .mt-overlay').toggleClass('active');
+        // $('.mt-header__menu ul').slideToggle();
+    });
+
+    //mobile sub menu
+    $('.mt-header__menu .menu-item-has-children > a').each(function () {
+        $(this).after("<span class='sub-toggle'><i class='mt-icon icon-arrow_right'></i></span>");
+    });
+    $(document).on('click', '.sub-toggle', function () {
+        $(this).toggleClass('active').next('ul').slideToggle();
+    });
+
+    $(document).on('click', '.mt-mini-cart-open ', function () {
+        $('body').addClass('mt-mini-cart-active');
+        $('.mt-mini-cart').addClass('active');
+        $('.mt-overlay').addClass('active');
+        $('.mt-overlay').css({ "z-index": "7" });
+    });
+    $(document).on('click', '.mt-cart-close ', function () {
+        $('body').removeClass('mt-mini-cart-active');
+        $('.mt-mini-cart').removeClass('active');
+        $('.mt-overlay').removeClass('active');
+        $('.mt-overlay').css({ "z-index": "5" });
+    });
+    $(document).on('click', '.mt-overlay', function () {
+        $('body').removeClass('mt-mini-cart-active');
+        $('.mt-mini-cart').removeClass('active');
+        $('.mt-overlay').removeClass('active');
+        $('.ham-menu').removeClass('active');
+        $('.mt-header__wrapper').removeClass('active');
+    });
+    $(document).on('click', '.mt-product-menu .mt-section-heading', function () {
+        $('.mt-product-filter').slideToggle();
+        $('.mt-product-menu .mt-section-heading').toggleClass('active');
+    });
+
+    $('.mt-billboard-slider').slick({
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        speed: 400,
+        infinite: true,
+        cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+        touchThreshold: 100
+    });
+
+    $('.mt-product-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: false,
+        arrows: false,
+        slidesToShow: 5,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 2001,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    $('.mt-brand-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        arrows: false,
+        slidesToShow: 7,
+        slidesToScroll: 7,
+        responsive: [
+            {
+                breakpoint: 1350,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    // product detail
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.slider-for',
+        focusOnSelect: true,
+        vertical: true,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    vertical: false,
+                    variableWidth: true,
+
+                }
+            }
+        ]
+    });
+
+    // home banner
+    setTimeout(function () {
+        var get_height = $('video.same-height').height();
+        $('img.same-height').css("height", get_height);
+    }, 1000);
+    $(window).resize(function () {
+        var get_height = $('video.same-height').height();
+        $('img.same-height').css("height", get_height);
+    })
+
+});
+
+//toggle order summary
+$(document).on('click', '.mt-aside__head ', function () {
+
+    $('.mt-aside__body').slideToggle();
+
+});
